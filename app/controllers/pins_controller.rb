adbdc6b2752571b6da519897f9b02cbf697068a5
German Escobar <german.escobarc@gmail.com>
2015-09-19 07:41:24 -0500

    Work on users and pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,30 @@
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @pins = Pin.all.order('created_at DESC')
+  end
+
+  def show
+    @pin = Pin.find(params[:id])
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    pin = Pin.new(pin_params)
+    pin.user = current_user
+    if pin.save
+      redirect_to pin, notice: "El pin fue creado exitosamente"
+    else
+      render :new
+    end
+  end
+
+  private
+    def pin_params
+      params.require(:pin).permit(:title, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pins#index'
+
   resources :pins
 
   devise_for :users
